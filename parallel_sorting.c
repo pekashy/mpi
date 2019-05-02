89c7ef87ad7e8161c903a22d1610b1db07fe6908
pekashy <klever.kl@yandex.ru>
2019-05-02 18:21:31 +0300

    own barrier solution

--- parallel_sorting.c
+++ parallel_sorting.c
@@ -1,3 +1,4 @@
+
 /*
 Первая строка файла - количество сортируемых чисел (< 2147483647), вторая строка - набор чисел (в пределах int), который необходимо отсортировать. Программе через аргумент сообщается, сколько использовать потоков. После считывания файла программа сортирует числа на нужном количестве нитей и записывает результат в файл. В консоль программа выводит время работы алгоритма (от считывания файла и до вывода результата в файл).
 */
@@ -7,6 +8,7 @@
 #include <errno.h>
 #include <string.h>
 #include <semaphore.h>
+#include <sys/types.h>
 
 typedef struct mini_array{
     int* arr;
@@ -17,19 +19,25 @@ typedef struct mini_array{
     int total_num;
     int left_index_c;
     int left_index_n;
-    int* changed;
-    int* neigh_changed;
-
-    pthread_mutex_t* mutex; 
-    pthread_mutex_t* omutex; 
-    pthread_mutex_t** mutexes; 
-    pthread_mutex_t** omutexes; 
-    pthread_mutex_t* neigh_omutex; 
-    pthread_mutex_t* neigh_mutex; 
 
 } marr;
 
-sem_t finish_sem;
+sem_t mutex1;
+sem_t barrier1;
+sem_t mutex2;
+sem_t barrier2;
+sem_t mutex3;
+sem_t barrier3;
+sem_t mutex4;
+sem_t barrier4;
+sem_t mutex5;
+sem_t barrier5;
+
+int count1 = 0;
+int count2 = 0;
+int count3 = 0;
+int count4 = 0;
+int count5 = 0;
 
 void oddEvenSorting(int* array, int arrayLength) {
     int temp;
@@ -50,7 +58,6 @@ void print(int* arr, int len){
         printf("%d ", arr[a]);
     }
     printf("\n");
-    fflush(stdout);
 }
 
 int cmp_and_swap(int* a, int* b, int* changed){
@@ -83,66 +90,143 @@ int swap_to_end(int* arr, int len, int* changed){
     return i;
 }
 
-int* threadFunc(void* argm){
+void bar(marr* arg, int id){
+    if(id ==1){
+        count1 = count1 + 1;
+        sem_post(&mutex1);
+        if(count1 == arg->total_num) sem_post(&barrier1);
+        sem_wait(&barrier1);
+        sem_post(&barrier1); //# once we are unblocked, it's our duty to unblock the next thread
+    }
+    if(id == 2){
+        count2 = count2 + 1;
+        sem_post(&mutex2);
+        if(count2 == arg->total_num) sem_post(&barrier2);
+        sem_wait(&barrier2);
+        sem_post(&barrier2); //# once we are unblocked, it's our duty to unblock the next thread
+    }
+    if(id == 3){
+            count3 = count3 + 1;
+            sem_post(&mutex3);
+            if(count3 == arg->total_num) sem_post(&barrier3);
+            sem_wait(&barrier3);
+            sem_post(&barrier3); //# once we are unblocked, it's our duty to unblock the next thread
+    }
+    if(id == 4){
+            count4 = count4 + 1;
+            sem_post(&mutex4);
+            if(count4 == arg->total_num) sem_post(&barrier4);
+            sem_wait(&barrier4);
+            sem_post(&barrier4); //# once we are unblocked, it's our duty to unblock the next thread
+    }
+    if(id == 5){
+            count5 = count5 + 1;
+            sem_post(&mutex5);
+            if(count5 == arg->total_num) sem_post(&barrier5);
+            sem_wait(&barrier5);
+            sem_post(&barrier5); //# once we are unblocked, it's our duty to unblock the next thread
+    }
+
+}
+/*
+sem1, sem2 =0
+
+
+rank == 0
+    sem1 += p-1
+    sem2 -= p-1
+    
+rank != 0
+    sem2 += 1
+
+rank != 0
+    sem1 -= 1
+
+
+*/
+void threadFunc(void* argm){
     marr* arg = (marr*) argm;
-    sem_wait(&finish_sem); 
-    //printf("%d %d %d", arg->arr[0], arg->arr[1], arg->len);
     int unsorted = 1;
     int temp=0;
     int limit;
     if(arg->len < 2) return arg->arr;
     int* pointer;
-	for(int count =0; count < arg->total_len*4; count++) {
-        pointer = arg->start;
-        arg->changed = 0;
-        if(arg->len % 2 ==0){
-            if( arg->mutex) pthread_mutex_lock(arg->mutex); //E
-            if( arg->omutex) pthread_mutex_lock(arg->omutex);
-            cmp_and_swap(pointer, (pointer+1), &arg->changed);
-            //print(arg->arr, arg->total_len);
-            //print(arg->start, arg->len);
-            pointer +=2;
-            pthread_mutex_unlock(arg->mutex);
-            pointer += swap_to_end(pointer, arg->len - 1 , &arg->changed);
-            //print(arg->arr, arg->total_len);
-            //print(arg->start, arg->len);
-            if(arg->num == 0) 
-                pthread_mutex_unlock(arg->omutex);
-
-            pointer = arg->start + 1;
-            if( arg->omutex) pthread_mutex_lock(arg->omutex); //M
-            pointer += swap_to_end(pointer, arg->len -1 , &arg->changed);
-            if(arg->neigh_mutex)
-                pthread_mutex_lock(arg->neigh_mutex);
-            //print(arg->arr, arg->total_len);
-            //print(arg->start, arg->len);
-            if(arg->neigh_mutex){
-                if(cmp_and_swap(pointer, (pointer+1), &arg->changed))
-                    arg->neigh_changed = 1;
-            }
-            pointer+=2;
-            if(arg->neigh_mutex)
-                pthread_mutex_unlock(arg->neigh_mutex);
-            pthread_mutex_unlock(arg->omutex); 
-            if(arg->neigh_omutex)
-               pthread_mutex_unlock(arg->neigh_omutex); 
-            //fflush(stdout);
-            //print(arg->arr, arg->total_len);
-            //print(arg->start, arg->len);
-            //printf("cycle end c %d\n", arg->changed);
-        }
-        //pthread_mutex_destroy(arg->neigh_mutex);
-        //pthread_mutex_destroy(arg->neigh_omutex);
-        //arg->neigh_mutex = NULL;
-        //arg->neigh_omutex = NULL;
-	}
-    pthread_mutex_unlock(arg->mutex);
-    pthread_mutex_unlock(arg->omutex);
     fflush(stdout);
-    print(arg->arr, arg->total_len);
-    sem_post(&finish_sem); 
+    int changed;
+    pid_t tid = pthread_self();  
+    printf("%d %d\n", arg->num, arg->len);
+    for(int c = 0; c < arg->total_len; c++) {
 
-    return arg->arr;
+        if(arg->len%2 == 0){
+            pointer = arg-> start;
+            swap_to_end(pointer, arg->len, &changed);
+        }
+        else{
+            pointer = arg-> start;
+            if(arg->num < arg->total_num) swap_to_end(pointer, arg->len, &changed);
+            else swap_to_end(pointer, arg->len-1, &changed);
+        }
+
+
+        sem_init(&mutex3, 0, 1);
+        sem_init(&barrier3, 0, 0);
+        count3 = 0;
+            bar(arg, 1);
+        fflush(stdout);
+        //printf("barrier %d: %d 1\n", tid, c);
+        //printf("%d %d %d %d 1\n", count1, count2, count3, count4);
+        fflush(stdout);
+
+        sem_init(&mutex3, 0, 1);
+        sem_init(&barrier3, 0, 0);
+        count3 = 0;
+            bar(arg, 2);
+        fflush(stdout);
+        //printf("barrier %d: %d 2\n", tid, c);
+        //printf("%d %d %d %d 2\n", count1, count2, count3, count4);
+
+        fflush(stdout);
+
+        if(arg->len%2 == 0){
+            pointer = arg-> start + 1;
+            if(arg->num< arg->total_num) swap_to_end(pointer, arg->len, &changed);
+            else swap_to_end(pointer, arg->len-1, &changed);
+        }
+        else{
+            pointer = arg-> start + !arg->num%2;
+            fflush(stdout);
+            printf("%d\n", !arg->num%2);
+                    fflush(stdout);
+            if(arg->num< arg->total_num) swap_to_end(pointer, arg->len - !arg->num%2, &changed);
+            else swap_to_end(pointer, arg->len-1, &changed);
+        }
+
+
+
+        sem_init(&mutex1, 0, 1);
+        sem_init(&barrier1, 0, 0);
+        count1 = 0;
+        sem_init(&mutex4, 0, 1);
+        sem_init(&barrier4, 0, 0);
+        count4 = 0;
+            bar(arg, 3);
+        fflush(stdout);
+        fflush(stdout);
+       // printf("barrier %d: %d 3\n", tid, c);
+        //printf("%d %d %d %d 3\n", count1, count2, count3, count4);
+        fflush(stdout);
+
+        sem_init(&mutex2, 0, 1);
+        sem_init(&barrier2, 0, 0);
+        count2 = 0;
+            bar(arg, 4);
+        fflush(stdout);
+        fflush(stdout);
+        //printf("barrier %d: %d 4\n", tid, c);
+        //printf("%d %d %d %d 4\n", count1, count2, count3, count4);
+        //print(arg->arr, arg->total_len);
+        fflush(stdout);
+    }
 }
 
 
@@ -161,40 +245,34 @@ int main(int argc, char** argv){
     marr* arrs = calloc(nthreads, sizeof(marr));//allocating memory
     int* ret[nthreads];
     pthread_t threads[nthreads];
-    pthread_mutex_t* mutexes = calloc(nthreads, sizeof(pthread_mutex_t));
-    pthread_mutex_t* omutexes = calloc(nthreads, sizeof(pthread_mutex_t));
-    
-    sem_init(&finish_sem, 0, nthreads);
+
+    sem_init(&mutex1, 0, 1);
+    sem_init(&barrier1, 0, 0);
+    sem_init(&mutex2, 0, 1);
+    sem_init(&barrier2, 0, 0);
+    sem_init(&mutex3, 0, 1);
+    sem_init(&barrier3, 0, 0);
+    sem_init(&mutex4, 0, 1);
+    sem_init(&barrier4, 0, 0);
+    sem_init(&mutex5, 0, 1);
+    sem_init(&barrier5, 0, 0);
 
     for(int t=0; t < nthreads; t++){
         arrs[t].num = t;
         arrs[t].total_num = nthreads;
         arrs[t].total_len = len;
-        arrs[t].changed = &changed[t];
-        *arrs[t].changed = 1;
-        arrs[t].mutex = &mutexes[t];
-        arrs[t].omutex = &omutexes[t];
-        pthread_mutex_init(arrs[t].mutex, NULL);
-        pthread_mutex_init(arrs[t].omutex, NULL);
 
         if(t < nthreads-1){ 
             arrs[t].len = len/nthreads;
         }
         else{
             arrs[t].len = len - (len/nthreads)*t;
-            arrs[t].neigh_omutex = NULL;
-            arrs[t].neigh_mutex = NULL;
         }
-        if(t > 0){
-            arrs[t-1].neigh_omutex = arrs[t].omutex;
-            arrs[t-1].neigh_mutex = arrs[t].mutex;
-            arrs[t-1].neigh_changed = arrs[t].changed;
-        }
-        
         arrs[t].arr = array;//calloc(len, sizeof(int));
         arrs[t].start = array + t*arrs[t].len;//calloc(len, sizeof(int));
         arrs[t].left_index_c = t*(len/nthreads+1);
         arrs[t].left_index_n = arrs[t].left_index_c+1;
+        
     }
 
     printf("loaded\n");
@@ -204,14 +282,13 @@ int main(int argc, char** argv){
         }
     }
 
-    /*oddEvenSorting(arrs[0].arr, arrs[0].len);
-    memcpy(ret[0], arrs[0].arr, arrs[0].len*sizeof(int));*/
 
-    threadFunc(&arrs[0]);
+    threadFunc(arrs);
 
-    sem_wait(&finish_sem); 
     fflush(stdout);
-    //print(array, len);
+    print(array, len);
+
+
 
     /*int piece;
     for(int a=0; a < nthreads; a++){ // printing results
